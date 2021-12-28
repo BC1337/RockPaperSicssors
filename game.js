5f920741b7c6a890b3dbb12d5d761a10600f51ac
Brett <brett_comrie@hotmail.com>
2021-12-28 14:59:53 -0600

    redid the whole project, added user GUI, HTML & CSS

--- game.js
+++ game.js
@@ -1,56 +1,84 @@
-let playerScore = 0
-let computerScore = 0
-const buttons = document.querySelectorAll('input')
+let userScore = 0;
+let computerScore = 0;
 
-function computerPlay() {
-    let choices = ['rock', 'paper', 'scissors']
-    return choices[Math.floor(Math.random() * choices.length)]
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+function getComputerChoice(){
+    const choices = ['r', 'p', 's'];
+    const randomNumber = (Math.floor(Math.random()* 3));
+    return choices[randomNumber];
 }
 
-function disableButtons() {
-    buttons.forEach(elem => {
-        elem.disabled = true
-    })
+function convertToWord(letter){
+    if (letter === 'r') return "Rock";
+    if (letter === 'p') return "Paper";
+    return "Scissors";
 }
 
-function playRound(playerSelection) {
-    let computerSelection = computerPlay()
-    let result = ""
-
-    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
-        (playerSelection == 'scissors' && computerSelection == 'paper') ||
-        (playerSelection == 'paper' && computerSelection == 'rock')) {
-        
-        playerScore += 1
-        result = ('You win! ' + playerSelection + ' beats ' + computerSelection
-            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
-
-        if (playerScore == 5) {
-            result += '<br><br>You won the game! Reload the page to play again'
-            disableButtons()
-        }
-    }
-    else if (playerSelection == computerSelection) {
-        result = ('It\'s a tie. You both chose ' + playerSelection
-            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
-    }
-    else {
-        computerScore += 1
-        result = ('You lose! ' + computerSelection + ' beats ' + playerSelection
-            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
-
-        if (computerScore == 5) {
-            result += '<br><br>I won the game! Reload the page to play again'
-            disableButtons()
-        }
-    }
-
-    document.getElementById('result').innerHTML = result
-    return
+function win(userChoice, computerChoice){
+    const userChoice_div = document.getElementById(userChoice);
+   userScore++;
+   userScore_span.innerHTML = userScore;
+   computerScore_span.innerHTML = computerScore;
+   result_p.innerHTML = `${convertToWord(userChoice)} beats ${convertToWord(computerChoice)}. You win!`
+   userChoice_div.classList.add('green-glow');
+   setTimeout(() => userChoice_div.classList.remove('green-glow'), 400);
+}
+function lose(userChoice, computerChoice){
+    const userChoice_div = document.getElementById(userChoice);
+    computerScore++;
+   userScore_span.innerHTML = userScore;
+   computerScore_span.innerHTML = computerScore;
+   result_p.innerHTML = `${convertToWord(userChoice)} loses to ${convertToWord(computerChoice)}. You lost...`
+   userChoice_div.classList.add('red-glow');
+   setTimeout(() => userChoice_div.classList.remove('red-glow'), 400);
+}
+function draw(userChoice, computerChoice){
+    const userChoice_div = document.getElementById(userChoice);
+   result_p.innerHTML = `${convertToWord(userChoice)} equals ${convertToWord(computerChoice)}. It's a draw.`
+   userChoice_div.classList.add('gray-glow');
+   setTimeout(() => userChoice_div.classList.remove('gray-glow'), 400);
 }
 
-buttons.forEach(button =>{
-    button.addEventListener('click', function(){
-        playRound(button.value)
-    })
-})
+
+function game(userChoice){
+const computerChoice = getComputerChoice();
+switch (userChoice + computerChoice){
+    case "rs":
+    case "pr":
+    case "sp":
+        win(userChoice, computerChoice);
+        break;
+    case "rp":
+    case "ps":
+    case "sr":
+        lose(userChoice, computerChoice);
+        break;
+    case "rr":
+    case "pp":
+    case "ss":
+         draw(userChoice, computerChoice);
+         break;
+}
+}
+
+
+
+
+function main(){
+    rock_div.addEventListener('click',() => game("r"));
+     
+     paper_div.addEventListener('click', () => game("p"));
+     
+     
+     scissors_div.addEventListener('click', () => game("s"));   
+}
+
+main();
+
